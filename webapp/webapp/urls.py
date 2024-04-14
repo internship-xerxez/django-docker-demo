8a36e66f752d783c8ab3c8b3fc43a75e9c12a7e8
Xerxez Solutions <info@xerxez.in>
2024-04-14 19:51:50 +0530

    Second Commit

--- webapp/webapp/urls.py
+++ webapp/webapp/urls.py
@@ -17,8 +17,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from core import views
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
-    #path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', views.index, name="index"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
